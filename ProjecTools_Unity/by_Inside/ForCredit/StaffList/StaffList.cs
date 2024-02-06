756c578709363a469bb78c608fc57d9d0b2dca71
Shima <>
2024-02-06 12:43:00 +0900

    no message

--- ProjecTools_Unity/by_Inside/ForCredit/StaffList/StaffList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[ExcelAsset]
-public class StaffList : ScriptableObject
-{
-    public List<StaffListVariable> list;
-}
